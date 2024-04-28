3e165370cb68a87238614e5e0e4884d4cda7daad
Andrei Gudkov <andygudkov@gmail.com>
2024-04-28 18:43:51 +0400

    Second Task

--- QAP_18_HW3.py
+++ QAP_18_HW3.py
@@ -6,3 +6,13 @@ def to_int(arr):
 
 print(to_int(1.6))
 print(to_int(2.99))
+
+# 2 Заменить символ “#” на символ “/” в строке 'www.my_site.com#about'
+str_my_site = "www.my_site.com#about"
+
+
+def replace_sharp_by_slash(site):
+    return site.replace('#', '/')
+
+
+print(replace_sharp_by_slash(str_my_site))
