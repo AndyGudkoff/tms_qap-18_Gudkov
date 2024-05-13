0143956b69d343c54b108d7475a21a97d0159f70
Andrei Gudkov <andygudkov@gmail.com>
2024-05-14 02:54:10 +0400

    SecondTask

--- QAP18_HW6.py
+++ QAP18_HW6.py
@@ -15,3 +15,30 @@ if len(listik) < 3:
     print("Error")
 else:
     print(listik[0], listik[1], listik[len(listik)-2])
+
+
+# 2 Дан файл целых чисел. Создать два новых файла, первый из которых
+# содержит четные числа из исходного файла, а второй — нечетные (в том
+# же порядке). Если четные или нечетные числа в исходном файле
+# отсутствуют, то соответствующий результирующий файл оставить
+# пустым.
+
+
+date_from_file = read_file('file_with_ints.txt')
+list_with_all_integers = date_from_file.split()
+
+odd = []
+ints = []
+for i in list_with_all_integers:
+    if int(i) % 2 == 0:
+        ints.append(i)
+    else:
+        odd.append(i)
+
+file1 = open('FileWithIntegers.txt', 'w')
+file1.write(str(ints))
+file1.close()
+
+file2 = open('FileWithOddNumbers.txt', 'w')
+file2.write(str(odd))
+file2.close()
